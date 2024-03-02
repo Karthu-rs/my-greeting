83eda98edb845b9f966e8f14f83dc51534a0b92f
Karthu-rs <harishmarsnair@gmail.com>
2024-03-02 13:47:00 +0530

    init

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const spinningCube = document.getElementById('spinning-cube');
+
+//     // Pause the rotation when the mouse enters the cube
+//     spinningCube.addEventListener('mouseenter', function () {
+//         spinningCube.style.animationPlayState = 'paused';
+//     });
+
+//     // Resume the rotation when the mouse leaves the cube
+//     spinningCube.addEventListener('mouseleave', function () {
+//         spinningCube.style.animationPlayState = 'running';
+//     });
+// });
+
+// const spinningCube = document.getElementById("spinning-cube");
+
+// spinningCube.addEventListener('click', () => {
+//     spinningCube.style.animationPlayState = 'paused'
+    
+//     setTimeout(() => {
+//     spinningCube.style.animationPlayState = 'running'
+//     },1000)
+// })
